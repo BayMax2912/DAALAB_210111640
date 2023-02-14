8e5303e5b3aac39cc2ff319348954aeb8b48c42d
BayMax2912 <85914911+BayMax2912@users.noreply.github.com>
2023-02-14 13:47:25 +0530

    Add files via upload

---
+++ q1.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+int t;
+cin>>t;
+while(t--)
+{
+int s;
+int num;
+cin>>s;
+cin>>num;
+int a[s];
+for(int i=0;i<s;i++)
+{
+cin>>a[i];
+
+}
+int index;
+for(int i=0;i<s;i++)
+{
+if(a[i]==num)
+{
+index=i;
+break;
+}
+}
+cout<<"Present"<<index+1<<endl;
+
+}
+return 0;
+}
